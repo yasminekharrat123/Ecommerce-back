4daedf05d83b3fab8c48c6ca08e5521dcb44430b
Yasmine Kharrat <yasminekharrat1@gmail.com>
2023-02-17 13:15:20 +0100

    client service file

--- src/client/client.module.ts
+++ src/client/client.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ClientController } from './client.controller';
+import { ClientService } from './client.service';
 
 @Module({
-  controllers: [ClientController]
+  controllers: [ClientController],
+  providers: [ClientService]
 })
 export class ClientModule {}
