84db504ff4ec6188f75ae15c7c262b5b9523c8ed
Yasmine Kharrat <yasminekharrat1@gmail.com>
2023-02-17 13:10:35 +0100

    test

--- src/client/entities/client.entity.ts
+++ src/client/entities/client.entity.ts
@@ -1,24 +1,36 @@
-import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
-
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  JoinColumn,
+  OneToOne,
+} from 'typeorm';
 
 @Entity({ name: 'client', schema: 'public' })
-export class client{
+export class client {
+  @PrimaryGeneratedColumn()
+  idClient: number;
 
-    @PrimaryGeneratedColumn()
-    idClient: number;
+  @Column()
+  name: string;
 
-    @Column()
-    name: string;
+  @Column()
+  email: string;
 
-    @Column()
-    email: string;
+  @Column()
+  description: string;
 
-    @Column()
-    description: string;
+  @Column()
+  password: string;
 
-    @Column()
-    password: string;
+  @Column({ type: 'bytea', nullable: true })
+  image: Buffer;
 
-    @Column({ type: 'bytea', nullable: true })
-    image: Buffer;
+  @OneToOne(() => FavorisEntity)
+  @JoinColumn()
+  favoris: FavorisEntity;
+
+  @OneToOne(() => PanierEntity)
+  @JoinColumn()
+  panier: PanierEntity;
 }
