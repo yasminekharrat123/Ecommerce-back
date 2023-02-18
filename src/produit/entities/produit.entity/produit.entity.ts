dca3b07dfe2ec5df556f47d4b3bc47f9694ed855
Yasmine Kharrat <yasminekharrat1@gmail.com>
2023-02-18 15:49:30 +0100

    fixes

--- src/produit/entities/produit.entity/produit.entity.ts
+++ src/produit/entities/produit.entity/produit.entity.ts
@@ -2,7 +2,6 @@ import { FavorisEntity } from 'src/favoris/entities/favoris.entity';
 import { ProduitCommandeEntity } from './../../../ProduitCommande/entities/ProduitCommande.entity';
 
 import { TimestampEntities } from '../../Generics/timestamp.entities';
-import { PanierEntity } from 'src/panier/entities/panier.entity';
 import {
   Column,
   Entity,
@@ -32,9 +31,7 @@ export class ProduitEntity extends TimestampEntities {
   @Column({ type: 'int' })
   quantite: number;
 
-
   @ManyToMany(() => FavorisEntity)
   @JoinTable()
   favoris: FavorisEntity[];
-
 }
